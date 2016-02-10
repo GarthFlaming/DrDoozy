67f844d259e87c820e2a842c08804a874231dcab
DeathxPain107 <npecka@iastate.edu>
2016-02-10 11:15:23 -0600

    yes
    gooooood

--- workspace/mainGame/src/mainGame/test2.java
+++ workspace/mainGame/src/mainGame/test2.java
@@ -1,12 +1,13 @@
 package mainGame;
 
-public class testClass {
+public class test2 {
 
 	public static void main(String[] args) {
 		int i = 1;
 		int j = 2;
 		String fine = "wows"; 
 		System.out.println(fine + i + j);
+		System.out.println("goooooooood");
 	}
 
 }
