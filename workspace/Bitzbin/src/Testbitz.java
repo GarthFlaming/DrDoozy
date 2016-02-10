acda4932b808a25011f5432c1a5ba8e8eab0ef8f
rahxki <kosobuj@iastate.edu>
2016-02-10 10:57:16 -0600

    retry

--- workspace/Bitzbin/src/Testbitz.java
+++ workspace/Bitzbin/src/Testbitz.java
@@ -3,5 +3,6 @@ public class Testbitz {
 
 	public static void main(String[] args){
 		System.out.print("'Ey which button makes dis one go?");
+		System.out.print("'Ez still buggin out, boss.");
 	}
 }
