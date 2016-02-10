2d27bbc589ba81428330222339428e17003fbd4f
DeathxPain107 <npecka@iastate.edu>
2016-02-10 11:08:27 -0600

    Hopefully last
    last test yo

--- workspace/mainGame/src/mainGame/testClass.java
+++ workspace/mainGame/src/mainGame/testClass.java
@@ -5,6 +5,7 @@ public class testClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Why is so complicated");
+		System.out.println("welcome to java");
 	}
 
 }
