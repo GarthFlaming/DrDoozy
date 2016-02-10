559f577d74d8b3be786ca42b896b49636663320b
Flaming <gflaming@iastate.edu>
2016-02-10 11:08:52 -0600

    added test2.java inside workspace

---
+++ workspace/mainGame/src/mainGame/test2.java
@@ -0,0 +1,12 @@
+package mainGame;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		int i = 1;
+		int j = 2;
+		String fine = "wows"; 
+		System.out.println(fine + i + j);
+	}
+
+}
