4683c7ac432162fe1c0a5a2a2705ec93eb2409e9
Flaming <gflaming@iastate.edu>
2016-02-17 10:27:55 -0600

    added things to the Thisisavlass

--- workspace/mainGame/src/mainGame/Thisisavlass.java
+++ workspace/mainGame/src/mainGame/Thisisavlass.java
@@ -1,5 +1,8 @@
 package mainGame;
 
 public class Thisisavlass {
-
+	public static void main(String[] args){
+		System.out.println("thee oh to the yeah."); //the succ is strong
+		int i; //the memes are strong.
+	}
 }
