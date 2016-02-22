ebf49327cce0b0e37174ca19915b8d7a9fb8eb47
DeathxPain107 <npecka@iastate.edu>
2016-02-21 18:31:23 -0600

    First terrain
    Just implemented first terrain

--- workspace/mainGame/src/engineTester/MainGameLoop.java
+++ workspace/mainGame/src/engineTester/MainGameLoop.java
@@ -1,59 +1,61 @@
 package engineTester;
-
-import org.lwjgl.opengl.Display;
-import org.lwjgl.util.vector.Vector3f;
-
-import entities.Camera;
-import entities.Entity;
+ 
 import models.RawModel;
 import models.TexturedModel;
+ 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+ 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
+import terrains.Terrain;
 import textures.ModelTexture;
-
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+ 
 public class MainGameLoop {
-
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		DisplayManager.createDisplay();
-		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
-		
-		// OpenGL expects vertices to be defined counter clockwise by default
-		
-		
-		RawModel model = OBJLoader.loadObjModel("stall", loader);
-		
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
-		Camera camera = new Camera();
-		
-		
-		while(!Display.isCloseRequested()){
-			entity.increaseRotation(0,1,0);
-			camera.move();
-			renderer.prepare();
-			//game logic
-			//render
-			shader.start();
-			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
-			shader.stop();
-			DisplayManager.updateDisplay();
-		}
-		
-		shader.cleanUp();
-		loader.cleanUp();
-		DisplayManager.closeDisplay();
-
-	}
-
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+         
+         
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+         
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("white")));
+        ModelTexture texture = staticModel.getTexture();
+        texture.setShineDamper(8);
+        texture.setReflectivity(1f);
+         
+        Entity entity = new Entity(staticModel, new Vector3f(100,0,-100),0,0,0,1);
+        Light light = new Light(new Vector3f(3000,2000,2000),new Vector3f(1,1,1));
+        
+        Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+         
+        Camera camera = new Camera();
+        
+        MasterRenderer renderer = new MasterRenderer();
+        while(!Display.isCloseRequested()){
+            entity.increaseRotation(0, 1, 0);
+            camera.move();
+            
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+            renderer.processEntity(entity);
+            
+            renderer.render(light, camera);
+            DisplayManager.updateDisplay();
+        }
+ 
+        renderer.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
 }
